988b26e4e9ff480f8b5ec9b8950799c9ce51eb54
SteveJrong <1554052208@qq.com>
2019-04-17 23:45:49 +0800

    u

--- shells/deploy_mysql_with_docker.sh
+++ shells/deploy_mysql_with_docker.sh
@@ -14,7 +14,7 @@ echo -e '--------------------  Starting install MySQL.\n'
 
 # Pull MySQL docker image.
 echo '--------------------  Pull MySQL docker image.'
-docker pull hub.c.163.com/library/mysql:5.7.17
+docker pull daocloud.io/library/mysql:5.7.20
 sleep 3
 echo -e '--------------------  Done.\n'
 
@@ -33,9 +33,7 @@ fi
 
 # Run Nginx container instance in Docker.
 echo '--------------------  Run MySQL container instance in Docker.'
-echo "--------------------  MYSQL_\"$DMWD_MYSQL_ACCOUNT\"_PASSWORD=\"$DMWD_MYSQL_DEFAULT_PWD\""
-docker run -p $DMWD_MYSQL_PORT:$DMWD_MYSQL_PORT --name mysql-main -e MYSQL_"$DMWD_MYSQL_ACCOUNT"_PASSWORD="$DMWD_MYSQL_DEFAULT_PWD" -d hub.c.163.com/library/mysql:5.7.17
-
+docker run --name mysql-main -p $DMWD_MYSQL_PORT:$DMWD_MYSQL_PORT -e MYSQL\_$DMWD_MYSQL_ACCOUNT\_PASSWORD=$DMWD_MYSQL_DEFAULT_PWD -d daocloud.io/library/mysql:5.7.20
 echo -e "--------------------  Done.\n"
 sleep 3
 # ###################################### Install MySQL end ######################################
